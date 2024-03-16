ecb9436bbb2d2021c71de1c4999c04fe2a3d7df0
abdulalkout <abdul.alkout@gmail.com>
2024-03-15 21:55:38 -0500

    footer style 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,8 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2024 Abdul Protfolio. All rights reserved.</p>
+        <p>&copy; 2024 Abdul Protfolio.</p>
         <div className="social-icons">
-          {/* Add your social media icons or links here */}
-          {/* Example: */}
-          {/* <a href="#" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-twitter"></i>
-          </a> */}
           <a
             href="https://www.linkedin.com/in/abdul-alkout/"
             target="_blank"
